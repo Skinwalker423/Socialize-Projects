0ebd7b16c3e83cf6cef54f9efa33376fceabad75
Luis Gonzalez <lgonzalez23@verizon.net>
2023-06-26 14:18:04 -0700

    navbar user avatar and authproviders

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,8 +2,11 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { NavLinks } from "@/constants";
+import AuthProviders from "./AuthProviders";
 
 const NavBar = () => {
+  const session = true;
+
   const renderedLinks = NavLinks.map(
     ({ href, text, key }) => {
       return (
@@ -16,18 +19,34 @@ const NavBar = () => {
 
   return (
     <nav className='flexBetween navbar'>
-      <div className='flex-1 flexStart gap-10'>
+      <div className='flexStart gap-10'>
         <Link href={"/"}>
           <Image
             src={"/slogo.svg"}
-            width={200}
+            width={250}
             height={50}
+            priority
             alt='Socialize logo'
           />
         </Link>
         <ul className='xl:flex hidden gap-10'>
           {renderedLinks}
         </ul>
+      </div>
+      <div className='flex-1 flexCenter gap-4'>
+        {session ? (
+          <>
+            <Image
+              src={"/vercel.svg"}
+              alt='user avatar'
+              width={50}
+              height={50}
+            />
+            <button>Share work</button>
+          </>
+        ) : (
+          <AuthProviders />
+        )}
       </div>
     </nav>
   );
