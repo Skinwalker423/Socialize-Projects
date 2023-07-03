a66fc1c4062e4270b88f109a8aba4f4dd9bbaf8c
Luis Gonzalez <lgonzalez23@verizon.net>
2023-07-03 12:33:16 -0700

    jwt encoding/decoding

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -12,6 +12,7 @@ import { g, auth, config } from "@grafbase/sdk";
 // Define Data Models
 // https://grafbase.com/docs/database
 
+//@ts-ignore
 const Project = g
   .model("Project", {
     title: g.string().length({ min: 3 }),
@@ -22,33 +23,47 @@ const Project = g
     category: g.string().search(),
     createdBy: g.relation(() => User),
   })
-  .search();
+  .search()
+  .auth((rules) => {
+    rules.public().read(),
+      rules.private().create().delete().update();
+  });
 
-const User = g.model("User", {
-  name: g.string().length({ min: 2, max: 20 }),
-  email: g.email().unique(),
-  avatarUrl: g.string().optional(),
-  description: g.string().optional(),
-  gitHubUrl: g.url().optional(),
-  linkedInUrl: g.url().optional(),
-  project: g
-    .relation(() => Project)
-    .list()
-    .optional(),
+//@ts-ignore
+const User = g
+  .model("User", {
+    name: g.string().length({ min: 2, max: 20 }),
+    email: g.email().unique(),
+    avatarUrl: g.string().optional(),
+    description: g.string().optional(),
+    gitHubUrl: g.url().optional(),
+    linkedInUrl: g.url().optional(),
+    project: g
+      .relation(() => Project)
+      .list()
+      .optional(),
 
-  // Extend models with resolvers
-  // https://grafbase.com/docs/edge-gateway/resolvers
-  // gravatar: g.url().resolver('user/gravatar')
+    // Extend models with resolvers
+    // https://grafbase.com/docs/edge-gateway/resolvers
+    // gravatar: g.url().resolver('user/gravatar')
+  })
+  .auth((rules) => {
+    rules.public().read();
+  });
+
+const jwt = auth.JWT({
+  issuer: "grafbase",
+  secret: g.env("NEXTAUTH_SECRET"),
 });
 
 export default config({
   schema: g,
   // Integrate Auth
   // https://grafbase.com/docs/auth
-  // auth: {
-  //   providers: [authProvider],
-  //   rules: (rules) => {
-  //     rules.private()
-  //   }
-  // }
+  auth: {
+    providers: [jwt],
+    rules: (rules) => {
+      rules.private();
+    },
+  },
 });
--- lib/session.ts
+++ lib/session.ts
@@ -32,10 +32,28 @@ export const authOptions: NextAuthOptions = {
     }),
     // ...add more providers here
   ],
-  // jwt: {
-  //   encode: ({ secret, token }) => {},
-  //   decode: async ({ secret, token }) => {},
-  // },
+  jwt: {
+    encode: ({ secret, token }) => {
+      const encodedToken = jsonwebtoken.sign(
+        {
+          ...token,
+          iss: "grafbase",
+          exp: Math.floor(Date.now() / 1000) + 60 * 60,
+        },
+        secret
+      );
+
+      return encodedToken;
+    },
+    decode: async ({ secret, token }) => {
+      const decodedToken = jsonwebtoken.verify(
+        token!,
+        secret
+      ) as JWT;
+
+      return decodedToken;
+    },
+  },
   secret: process.env.NEXTAUTH_SECRET,
   theme: {
     colorScheme: "dark",
